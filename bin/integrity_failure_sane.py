9a38d2f2e64c1af59521be23de1a80b9b178dc67
Tony Allen <tony@upbound.io>
2018-04-18 11:48:53 -0700

    Remove old Nutanix scripts.
    Signed-off-by: Tony Allen <tony@upbound.io>

--- bin/integrity_failure_sane.py
+++
@@ -1,6 +0,0 @@
-import sys, os
-
-assert(len(sys.argv) > 1)
-
-failure = raw_input("Paste the integrity \"Check failed:\" output: ")
-print ww
--- bin/load_generation/fio_tests/mk_seq_stats.sh
+++
@@ -1,3 +0,0 @@
-mkdir -p ~/tony_stats ;
-
-curl http://localhost:2009/rpc_stats/reset ; sshpass -p nutanix/4u ssh 192.168.5.253 "sh /home/nutanix/tony_seq.sh 6 240 $1" ; sleep 30 ; curl http://localhost:2009/rpc_stats/raw > ~/tony_stats/$1_seq.out ;
